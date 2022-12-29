fe43593a1092d5b3c44c6adc0392ea7db30a0ca5
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-12-29 11:40:04 +0600

    added extra section2

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
+import WhatIdo from '../WhatIdo/WhatIdo';
 
 const Home = () => {
     return (
@@ -14,6 +15,7 @@ const Home = () => {
             <div className='text-center my-12'>
                 <Link to='/services' className="btn btn-sm sm:btn-sm md:btn-md lg:btn-md bg-yellow-500 text-white text-lg  hover:text-white border-none hover:drop-shadow-2xl hover:bg-cyan-800">See All<FaCaretRight /></Link>
             </div>
+            <WhatIdo></WhatIdo>
         </div>
     );
 };
---
+++ src/Pages/Home/WhatIdo/WhatIdo.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const WhatIdo = () => {
+    return (
+        <div>
+            <h2 className='text-center mt-24 text-5xl font-semibold   '>What
+                <span className='text-cyan-600 text-6xl'> i </span>
+                Do ?</h2>
+            <div
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center lg:w-5/6 md:w-5/6 sm:w-full mx-auto mt-24"
+            >
+                <div className='card-body items-center text-center'>
+                    <img className='w-16 md:w-20 lg:w-24' src="https://www.talon.com.bd/assets/images/icon/Online-Booking-Service-icon-2.png" alt="img" />
+                    <h3 className=' mt-1 font-semibold text-cyan-800 text-lg md:text-xl lg:text-2xl'>Online Booking Service
+                    </h3>
+                    <p className='text-black text-semibold'>Online booking service through my OTA portal Tlntrip will have multi GDS connectivity and API</p>
+                </div>
+                <div className='card-body items-center text-center'>
+                    <img className='w-16 md:w-20 lg:w-24' src="https://www.talon.com.bd/assets/images/icon/Ticketing_2.png" alt="img" />
+                    <h3 className=' mt-1 font-semibold text-cyan-800 text-lg md:text-xl lg:text-2xl'>Ticketing
+                    </h3>
+                    <p className='text-black text-semibold'>Yes, i provide 24/7 ticketing service 365 days.</p>
+                </div>
+                <div className='card-body items-center text-center'>
+                    <img className='w-16 md:w-20 lg:w-24' src="https://www.talon.com.bd/assets/images/icon/Tour-Package.png" alt="img" />
+                    <h3 className=' mt-1 font-semibold text-cyan-800 text-lg md:text-xl lg:text-2xl'>Tour Package
+                    </h3>
+                    <p className='text-black text-semibold'>my experience will allow you to customize package accordingly to ymy need.</p>
+                </div>
+                <div className='card-body items-center text-center'>
+                    <img className='w-16 md:w-20 lg:w-24' src="https://www.talon.com.bd/assets/images/icon/Hotel-Booking-icon.png" alt="img" />
+                    <h3 className=' mt-1 font-semibold text-cyan-800 text-lg md:text-xl lg:text-2xl'>Hotel Booking
+                    </h3>
+                    <p className='text-black text-semibold'>Ymy 360 hotel booking provider.</p>
+                </div>
+                <div className='card-body items-center text-center'>
+                    <img className='w-16 md:w-20 lg:w-24' src="https://www.talon.com.bd/assets/images/icon/Foregin-Dollar-Endosement-icon.png" alt="img" />
+                    <h3 className=' mt-1 font-semibold text-cyan-800 text-lg md:text-xl lg:text-2xl'>Foregin Dollar Endosement
+                    </h3>
+                    <p className='text-black text-semibold'>Travel service for ymy at doorstep.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhatIdo;
