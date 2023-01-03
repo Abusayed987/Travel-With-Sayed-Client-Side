d73859fd5acdd07e17a30e661abc119cb247ea80
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-03 16:24:27 +0600

    create 404 route

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useTittle from '../../hooks/UseTittle';
 
 const NotFound = () => {
     useTittle('404')
     return (
-        <div>
-            <h2>404</h2>
+        <div className=''>
+            <img
+                className=' w-1/2 mx-auto my-20'
+                src="https://sharetrip.net/assets/images/no_page_found_screen_illustration_1x.png" alt="" />
+            <div className=' text-center'>
+                <h3 className='text-2xl font-semibold'>Page not found</h3>
+                <p className='my-4'>Sorry, the content that you are looking for doesn't exist.</p>
+                <Link to='/' className='btn btn-sm rounded-sm  bg-cyan-700 text-white   hover:text-white border-none hover:drop-shadow-2xl hover:bg-cyan-800  '>Go Home</Link>
+            </div>
         </div>
     );
 };
